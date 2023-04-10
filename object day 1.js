7237756929a9e1cb77bd06f4f70476df77d79a2d
Anthoni Duong <aduong@LABA.PSWAS.LOCAL>
2023-04-10 16:08:39 -0700

    objects assignment day 1

---
+++ object day 1.js
@@ -0,0 +1,93 @@
+// Write this in a JS file and submit it directly OR in replit and submit the link.
+
+ 
+
+// Problem 1
+
+// Declare a JS object called carThree with
+// A property called wheels with a number value
+// A property called owner with a string value
+// A property called isBroken with a boolean value
+// A property called color with a string value
+// Use dot notation to add a new property called price with a number value
+// Use bracket notation to add a new property called year with a number value
+// Use dot notation to change the value of the isBroken property
+// Use dot notation to read the value of owner
+// Create a new variable (not part of the object) called clientIsInterested that has no value.
+// Check if car color is blue. If true, set clientIsInterested to true, else false.
+// Delete the owner property of carThree.
+
+let carThree = {
+    wheels : 4,
+    owner: "Tony",
+    isBroken : false,
+    color : 'white'
+}
+    carThree.price = 10000,
+    carThree['year'] = 2023,
+    carThree.isBroken = true,
+    carThree.owner,
+    let clientIsInterested = " ",
+    clientIsInterested === carThree.color === "blue" ? true : false
+    delete carThree.owner
+    console.log(carThree),
+    console.log(clientIsInterested)
+
+// Problem 2
+
+// Declare a JS object called store with
+let store = {
+    profit = 100,
+    manager = 'Tony',
+    isOpenNow = true,
+    isVeryExpensive = false,
+}
+// A property called profit with a number value
+// A property called manager with a string value
+// A property called isOpenNow with a boolean value
+// A property called isVeryExpensive with a boolean value
+// Use dot notation to add a new property called location with a string value
+    store.location = 206
+// Use bracket notation to add a new property called storeType with a string value
+    store[storeType] = "corner store"
+// Use dot notation to change the value of the isOpenNow property
+    store.isOpenNow = false
+// Use dot notation to read the value of isVeryExpensive 
+    store.isVeryExpensive = true
+// Create a new variable (not part of the object) called canShopHere that has no value.
+    let canShopHere = " "
+// Check if isOpenNow and NOT isVeryExpensive. If true set canShopHere to true, else false
+    canShopHere === store.isOpenNow != store.isVeryExpensive ? true : false 
+// Delete the profit property of store.
+  delete store.profit 
+  console.log(store.profit)
+  
+
+// Problem 3
+
+ 
+
+// Declare a JS object called home with
+// A property called rooms with a number value
+// A property called bathrooms with a number value
+// A property called isForSale with a boolean value
+// A property called isInGoodLocation with a boolean value
+// Use dot notation to add a new property called price with a number value
+// Use bracket notation to add a new property called year with a number value
+// Use dot notation to change the value of the isForSale property
+// Use dot notation to read the value of bathrooms
+// Create a new variable (not part of the object) called buyHouse that has no value.
+// Check if isForSale and isInGoodLocation. If true set buyHouse to true, else false
+// Delete the isForSale property of home.
+ 
+
+// Problem 4, 5
+
+// Declare a JS object with 4 properties (4 keys and 4 values).
+// Use dot notation to add a new property.
+// Use bracket notation to add a new property.
+// Use dot notation to change the value of one of the properties.
+// Use dot notation to read the value of one of the properties.
+// Create a new variable (not part of the object) that has no value.
+// Write some kind of if / else statement to check some information of the object. Based on true / false set the value of the variable you made in number 6 to true or false.
+// Delete one of the object properties.
